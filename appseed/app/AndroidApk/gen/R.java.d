d28b037451aaa2d56a84904e5df77ba9f03104c8
ca2 <camilo@ca2.email>
2013-08-29 20:59:09 +0000

    nordroid/Android/NDK contribution!!
    Eu te amo Thomas Borregaard Sørensen!!
    
    Mummi contribution!!!! Feliz 62 anos!!
    
    at inha at veriverse - at Dell XPS 8300 Intel(R) Core(TM) i7-2600 CPU 4 core 8 threads @ 3.40GHz 16GB RAM 500GB + 500GB 7200rpm storage + 120 GB SSD OCZ Agility 3 with Windows 8 Pro - with Portuguese - Brazil- 10Mpbs Download and 2Mpbs Upload NET Virtua Internet with LG 23´´ monitor, Microsoft Comfort Curve 2000 v1.0 Wired Keyboard and Microsoft Comfort Optical Wired Mouse 3000 at Alameda Princesa Izabel, 3000 - Curitiba - PR - Brazil
    ｛
    ｝
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ appseed/app/AndroidApk/gen/R.java.d
@@ -0,0 +1,3 @@
+V:\laboratory\vs-android_samples\bitmap-plasma\AndroidApk\gen\com\example\plasma\R.java \
+ : V:\laboratory\vs-android_samples\bitmap-plasma\AndroidApk\res\values\strings.xml \
+V:\laboratory\vs-android_samples\bitmap-plasma\AndroidApk\AndroidManifest.xml \
