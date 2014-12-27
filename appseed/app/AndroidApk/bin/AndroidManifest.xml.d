1b901621aeafcac3e8e6ecddf3a1dcf0118aa418
ca2 <camilo@ca2.email>
2014-12-27 23:51:05 +0000

    Casey Muratori with Hand Made Hero contribution!! TimothyLottes contribution!! I am entering Ray tracing (going to reach enhanced sphere tracing)?!?! (Re-exists)(Re-inJavaByMySelfYearsAgoSamplesWithDinosaurRotation)!!!!! ï»¿nordroid (android) contribution lafter (long after in Nordroid - ops, Android - yeapi, theiis language....) ... from Z:\nordroid
    <3JegElskerDigThommy!!
    
    -âˆž
    +âˆž
    
    -âˆž
    æ—¥æœ¬ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™
    ï½›
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
       ã€€ã€€ã€€å¦»æ²¼å›½ç”·ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ï½
    
       ----
                            -   -   -
    DziÄ™kujÄ™ Polska!!
    
       Thomas Borregaard Sørensen dziÄ™kujÄ™!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    Mummi contribution!!, eu te amo demais!!
    +âˆž
    
    -âˆž
    +âˆž
    
    âˆž-âˆž-âˆžâˆž-âˆž-âˆž
    -----8nnnnnnnn8
    ++++++++++
    âˆž+âˆž+âˆžâˆž+âˆž+âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž

---
+++ appseed/app/AndroidApk/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\AndroidManifest.xml : \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\AndroidManifest.xml \
---
+++ appseed/app/AndroidApk/bin/classes.dex.d
@@ -0,0 +1,11 @@
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes.dex : \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\app_activity.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\app_view.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\ca2\android\app\BuildConfig.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\ca2\android\app\R$attr.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\ca2\android\app\R$string.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\ca2\android\app\R.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\com\example\plasma\R$attr.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\com\example\plasma\R$string.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\classes\com\example\plasma\R.class \
+Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\dexedLibs\annotations-62bab95d6948a2db17bbc7976160b014.jar \
