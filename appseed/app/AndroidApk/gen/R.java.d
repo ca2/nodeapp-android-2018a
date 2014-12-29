973f0bf052bd46b9eaea2b671c7c071e4f60c3c9
ca2 <camilo@ca2.email>
2014-12-29 07:34:10 +0000

    nordroid (android) contribution lafter... from Z:\nordroid
    <3JegElskerDigThommy!!
    
    ｛
    ｝
    
       ----
                            -   -   -
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/app/AndroidApk/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-Z:\nordroid\nodeapp\appseed\app\AndroidApk\gen\cc\ca2\androidapp\R.java \
- : Z:\nordroid\nodeapp\appseed\app\AndroidApk\res\values\strings.xml \
-Z:\nordroid\nodeapp\appseed\app\AndroidApk\bin\AndroidManifest.xml \
--- appseed/app/AndroidApk/gen/ca2/android/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ca2.android.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
