ed09e030a42abba674935d8885675fd58ce0ac99
ca2 <camilo@ca2.email>
2014-12-31 15:46:47 +0000

    nordroid (android) contribution lafter... from Z:\nordroid
    <3JegElskerDigThommy!!
    
    ｛
    ｝
    
       ----
                            -   -   -
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ appseed/app/AndroidApk/key.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Java\jdk1.7.0_55\bin\keytool.exe" -genkey -v -keystore ApplicationActivity.keystore -alias ApplicationActivity.androidapp.ca2.cc -keyalg RSA -keysize 2048 -validity 10000
