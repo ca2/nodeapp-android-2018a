76d5cdc6d4d883796182f6712643e6777731cbd5
ca2 <camilo@ca2.email>
2015-01-05 20:52:00 +0000

    From Visual Studio 2015 Preview prepared Virtual machine at inha veriverse!!

--- appseed/android.native_activity/main.cpp
+++ appseed/android.native_activity/main.cpp
@@ -25,7 +25,9 @@
 //
 
 #include "core/core/core.h"
+#include "base/user/user.h"
 #include "base/os/android/android.h"
+#include "base/node/android/android.h"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "AndroidProject1.NativeActivity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "AndroidProject1.NativeActivity", __VA_ARGS__))
