84480487c26704615c0130eddbafd7c5c2c2e5bc
ca2 <camilo@ca2.email>
2015-09-22 03:11:57 +0000

    Livecoding.tv contribution (Streamers: xmetrix, ThomasBS... contribution!!) (Pals: contribution: denvercowboy, user2 learning, epryka, dennisritchie, jamesgosling, Rukewetony, chocsx,  Cary, stefanosvchs, pe21, anti21, TonyCode, gordonmckkee, rondorules, jeffreims, barbare, ayvatan learning, watching, or just making "quorum" contribution...) ... contribution!! Twitter.com (Pals: contribution: Casey Muratori, Martin Cohen (with backround twits...) twits ... contribution ) ... contribution!!from hyperv (multiboot)!! Windows 10 contribution!!
    From Z:\nordroid at inha hyperv at veriverse!!
    ｛
    ｝
    
       ----
                            -   -   -
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- operational_system/config.h
+++ operational_system/config.h
@@ -1,8 +1,6 @@
 #pragma once
 
 
-
-#define __BSD_VISIBLE         1
 #define BSD_STYLE_SOCKETS
 #if !defined VARIADIC_TEMPLATE
 #define VARIADIC_TEMPLATE
@@ -34,32 +32,13 @@
 
 #ifndef ANDROID
 #define ANDROID
+#ifndef __arm__
 #define MD_SWAP
+#endif
 #define VSNORD // Visual Studio Nor-droid (Android)
 #endif
 
-#undef _GNU_SOURCE
-#define __STRICT_ANSI__ 1
-#define _POSIX_SOURCE 1
-#define _POSIX_C_SOURCE 200809L
-#ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE 700
-#endif
-#ifndef _XOPEN_SOURCE_EXTENDED
-#define _XOPEN_SOURCE_EXTENDED 1
-#endif
-#ifndef _LARGEFILE64_SOURCE
-#define _LARGEFILE64_SOURCE
-#endif
-#ifndef _SVID_SOURCE
-#define _SVID_SOURCE 1
-#endif
-
 #include <features.h>
-#undef _GNU_SOURCE
-#define _GNU_SOURCE 1
-#define __USE_GNU 1
-
 
 #define __WINESRC__
 
